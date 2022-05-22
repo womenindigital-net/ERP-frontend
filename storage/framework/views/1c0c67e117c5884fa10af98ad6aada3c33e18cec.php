bbeaf1526ef9eacc06e748bedbf40b5fa71268c2
Hridoy Roy <hridayroy05@gmail.com>
2022-05-22 17:37:54 +0600

    add Case Histroy Component Complcte

--- storage/framework/views/1c0c67e117c5884fa10af98ad6aada3c33e18cec.php
+++ storage/framework/views/1c0c67e117c5884fa10af98ad6aada3c33e18cec.php
@@ -30,9 +30,9 @@
                         <span>Income </span>
                     </a>
                     <ul class="sub-menu" aria-expanded="true">
-                        <li><a href="accounting.income.student_income" >Student Income</a></li>
-                        <li><a href="accounting.income.sale_voucher" >Sale Voucher</a></li>
-                        <li><a href="accounting.income.received_payment" >Received Payment</a></li>
+                        <li><a href="accounting.income.student_income">Student Income</a></li>
+                        <li><a href="accounting.income.sale_voucher">Sale Voucher</a></li>
+                        <li><a href="accounting.income.received_payment">Received Payment</a></li>
                     </ul>
                 </li>
                 <li>
@@ -42,9 +42,9 @@
                     </a>
                     <ul class="sub-menu">
                         <li><a href="accounting.purchase.requisition">Requisition</a></li>
-                        <li><a href="accounting.purchase.purchase_order" >Purchase Order</a></li>
-                        <li><a href="accounting.purchase.purchase_return" >Purchase Return</a></li>
-                        <li><a href="accounting.purchase.supplier_payment" >Supplier Payment</a></li>
+                        <li><a href="accounting.purchase.purchase_order">Purchase Order</a></li>
+                        <li><a href="accounting.purchase.purchase_return">Purchase Return</a></li>
+                        <li><a href="accounting.purchase.supplier_payment">Supplier Payment</a></li>
                         <li>
                             <a href="javascript: void(0);" class="has-arrow">Direct Payment</a>
                             <ul class="sub-menu">
@@ -55,10 +55,12 @@
                                     <a href="accounting.purchase.direct_payment.cash_payment_list">Cash Payment List</a>
                                 </li>
                                 <li>
-                                    <a href="accounting.purchase.direct_payment.advance_payment_list">Advance Payment List</a>
+                                    <a href="accounting.purchase.direct_payment.advance_payment_list">Advance Payment
+                                        List</a>
                                 </li>
                                 <li>
-                                    <a href="accounting.purchase.direct_payment.bank_to_bank_list">Bank to Bank Transfer List</a>
+                                    <a href="accounting.purchase.direct_payment.bank_to_bank_list">Bank to Bank Transfer
+                                        List</a>
                                 </li>
                             </ul>
                         </li>
@@ -70,13 +72,14 @@
                         <span>Inventory Management</span>
                     </a>
                     <ul class="sub-menu" aria-expanded="true">
-                        <li><a href="inventory-management.stock_assign" >Stock Assign</a></li>
-                        <li><a href="inventory-management.stock_out" >Stock Out</a></li>
-                        <li><a href="inventory-management.stock_hand_report" >Stock On Hand Report</a></li>
-                        <li><a href="inventory-management.location_wise_product_report" >Location Wise Product Report</a></li>
-                        <li><a href="inventory-management.stock_reorder_report" >Stock Reorder Report</a></li>
-                        <li><a href="inventory-management.stock_receive_report" >Stock receive report</a></li>
-                        <li><a href="product_analysis_report" >Report::Product Analysis</a></li>
+                        <li><a href="inventory-management.stock_assign">Stock Assign</a></li>
+                        <li><a href="inventory-management.stock_out">Stock Out</a></li>
+                        <li><a href="inventory-management.stock_hand_report">Stock On Hand Report</a></li>
+                        <li><a href="inventory-management.location_wise_product_report">Location Wise Product Report</a>
+                        </li>
+                        <li><a href="inventory-management.stock_reorder_report">Stock Reorder Report</a></li>
+                        <li><a href="inventory-management.stock_receive_report">Stock receive report</a></li>
+                        <li><a href="product_analysis_report">Report::Product Analysis</a></li>
                     </ul>
                 </li>
                 <li>
@@ -85,36 +88,35 @@
                         <span>Stock Management</span>
                     </a>
                     <ul class="sub-menu" aria-expanded="true">
-                        <li><a href="stock-management.stock_receive" >Stock Receive</a></li>
+                        <li><a href="stock-management.stock_receive">Stock Receive</a></li>
                         <li><a href="stock-management.finished_goods">Finished Goods</a></li>
-                        <li><a href="stock-management.material_callects" >Material Collects</a></li>
-                        <li><a href="stock-management.stock_transfer" >Stock Transfer</a></li>
-                        <li><a href="stock-management.stock_count" >Stock Count</a></li>
+                        <li><a href="stock-management.material_callects">Material Collects</a></li>
+                        <li><a href="stock-management.stock_transfer">Stock Transfer</a></li>
+                        <li><a href="stock-management.stock_count">Stock Count</a></li>
                     </ul>
                 </li>
                 <li class="menu-title">Program Modules</li>
-                
-                
-                
+
                 <li>
                     <a href="javascript: void(0);" class="has-arrow waves-effect">
                         <i class="bx bx-share-alt"></i>
                         <span>Pre Admission</span>
                     </a>
                     <ul class="sub-menu" aria-expanded="true">
-                        <li><a href="pre_admission.appointment">Appointment</a></li>
-                        <li><a href="/case-histroy">Case History</a></li>
+                        <li><a href="<?php echo e(route('student.appointment')); ?>">Appointment</a></li>
+                        <li><a href="<?php echo e(route('student.case-history')); ?>">Case History</a></li>
                         <li><a href="/care-needs-form">Care Needs Form</a></li>
                         <li><a href="/referral-form">Referral Form</a></li>
-                        
+                        <li><a href="/medicine-admin">Medicine Admin</a></li>
                     </ul>
                 </li>
                 <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                    <a href="admission.add_student" class="waves-effect">
                         <i class="bx bx-share-alt"></i>
                         <span>Admission</span>
                     </a>
                     
+                
                 </li>
                 <li>
                     <a href="javascript: void(0);" class="has-arrow waves-effect">
@@ -124,25 +126,45 @@
                     <ul class="sub-menu" aria-expanded="true">
                         <li><a href="assessment.sensory-checklist-for-child">Sensory checklist for Child</a></li>
                         <li><a href="assessment.Social-Communication">Social Communication</a></li>
-                        <li><a href="assessment.">Occupational Therapy</a></li>
-                        <li><a href="assessment.">Physiotherapy</a></li>
-                        <li><a href="assessment.">Functional Communication Assessment</a></li>
-                        <li><a href="assessment.">Fundamental Movement Skills</a></li>
-                        <li><a href="assessment.">Executive Function test</a></li>
-                        <li><a href="assessment.">Autism Behavior Checklist (ABC)</a></li>
-                        <li><a href="assessment.">Sensory checklist for Young Adult</a></li>
-                        <li><a href="assessment.">Individual risk assessment form</a></li>
+                        <li><a href="assessment.occupational-therapy">Occupational Therapy</a></li>
+                        <li><a href="assessment.physiotherapy">Physiotherapy</a></li>
+                        <li><a href="assessment.functional_communication_assessment">Functional Communication
+                                Assessment</a></li>
+                        <li><a href="assessment.functional_movement_skills">Fundamental Movement Skills</a></li>
+                        <li><a href="assessment.executive_function_test">Executive Function test</a></li>
+                        <li><a href="assessment.autism_behavior_checklist">Autism Behavior Checklist (ABC)</a></li>
+                        <li><a href="assessment.sensory_checklist_for_young_adult">Sensory checklist for Young Adult</a>
+                        </li>
+                        <li><a href="assessment.individual_risk_assessment_form">Individual risk assessment form</a>
+                        </li>
                     </ul>
                 </li>
                 <li>
                     <a href="javascript: void(0);" class="has-arrow waves-effect">
                         <i class="bx bx-share-alt"></i>
-                        <span >Student</span>
+                        <span>Student</span>
                     </a>
                     <ul class="sub-menu" aria-expanded="true">
-                        <li><a href="my_students" >Student List</a></li>
+                        <li><a href="my_students">Student List</a></li>
                         <li><a href="my-online-students">My Online Students</a></li>
-                        <li><a href="student-attendance-card-setup" >Student Attendance Card Setup</a></li>
+                        
+                        <li>
+                            <a href="javascript: void(0);" class="has-arrow">Co Curricular</a>
+                            <ul class="sub-menu" aria-expanded="true">
+                                <li> <a href="student.co-curricular.drama-diary-evaluation">Drama Diary Evaluation</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="javascript: void(0);" class="has-arrow">Employment</a>
+                            <ul class="sub-menu" aria-expanded="true">
+                                <li> <a href="student.employment.staff_and_workplace_inspecation_tool">কর্মী ও কর্মস্থল
+                                        পরিদর্শন টুল</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li><a href="student.vocational_evalution">Vocational Evalution</a></li>
+                        
                     </ul>
                 </li>
                 
@@ -157,9 +179,7 @@
                         <li><a href="program.run-mitp">Run MITP</a></li>
                         <li><a href="program.notice-board">Notice Board</a></li>
                         <li><a href="program.assign-teacher"> Assign Teacher</a></li>
-                        <li><a href="/">Score Calculator </a></li>
-                        <li><a href="/p-create-task">Create Task </a></li>
-                        <li><a href="/p-admission-request">Admission Request </a></li>
+                        
                     </ul>
                 </li>
                 
@@ -179,14 +199,16 @@
                         <li>
                             <a href="javascript: void(0);" class="has-arrow">Program Setup</a>
                             <ul class="sub-menu" aria-expanded="true">
-                                   <li> <a href="setup.program-setup.prescription">Prescription</a></li>
-                                   <li> <a href="setup.program-setup.activity-create">Activity create</a></li>
-                                   <li> <a href="setup.program-setup.tesk-create">Task create</a></li>
-                                   <li> <a href="setup.program-setup.tesk-weight-setup">Task Weight Setup</a></li>
-                                   <li> <a href="setup.program-setup.course-setup">Course Setup</a></li>
-                                   <li> <a href="setup.program-setup.attendence-card-setup">Attendence card setup</a></li>
-                                   <li> <a href="setup.program-setup.vocational-category-subcategory-setup">Vocational Category and subcategory setup</a></li>
-                                   <li> <a href="setup.program-setup.academic-calender-create">Academic Celender Create</a></li>
+                                <li> <a href="setup.program-setup.prescription">Prescription</a></li>
+                                <li> <a href="setup.program-setup.activity-create">Activity create</a></li>
+                                <li> <a href="setup.program-setup.tesk-create">Task create</a></li>
+                                <li> <a href="setup.program-setup.tesk-weight-setup">Task Weight Setup</a></li>
+                                <li> <a href="setup.program-setup.course-setup">Course Setup</a></li>
+                                <li> <a href="setup.program-setup.attendence-card-setup">Attendence card setup</a></li>
+                                <li> <a href="setup.program-setup.vocational-category-subcategory-setup">Vocational
+                                        Category and subcategory setup</a></li>
+                                <li> <a href="setup.program-setup.academic-calender-create">Academic Celender Create</a>
+                                </li>
                             </ul>
                         </li>
                         <li>
@@ -364,5 +386,4 @@
         <!-- Sidebar -->
     </div>
 </div>
-<!-- Left Sidebar End -->
-<?php /**PATH D:\GITHUB\ERP-frontend-WID\resources\views/layouts/sidebar.blade.php ENDPATH**/ ?>
+<!-- Left Sidebar End --><?php /**PATH D:\GITHUB\ERP-frontend-WID\resources\views/layouts/sidebar.blade.php ENDPATH**/ ?>
