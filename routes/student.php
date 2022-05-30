8109cc6a5393375cff620485021b27ae046e2640
sohag <shcseilacs@gmail.com>
2022-05-30 11:16:38 +0600

    added userseeder & modified student route

--- routes/student.php
+++ routes/student.php
@@ -2,16 +2,17 @@
 
 use App\Http\Controllers\StudentController;
 use App\Http\Livewire\AppointmentList;
+use App\Http\Livewire\CaseHistory;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'student', 'middleware' => []], function () {
+Route::group(['prefix' => 'student', 'middleware' => ['auth']], function () {
     Route::resources([
         '' => StudentController::class,
         //        'another' => \App\Http\Controllers\StudentController::class,
     ]);
 
     Route::get('appointment', AppointmentList::class)->name('student.appointment');
-    Route::get('case-history', [StudentController::class, 'caseHistory'])->name('student.case-history');
+    Route::get('case-history', CaseHistory::class)->name('student.case-history');
     Route::get('referral-form', [StudentController::class, 'referralForm'])->name('student.referral-form');
     Route::get('care-needs-form', [StudentController::class, 'careNeedForm'])->name('student.care-need-form');
 });
