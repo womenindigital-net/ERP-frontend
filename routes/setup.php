95c131e9e1cef08b6012e4d3f8ec90ccd2cb0b37
Hridoy Roy <hridayroy05@gmail.com>
2022-06-08 11:32:07 +0600

    setup

--- routes/setup.php
+++ routes/setup.php
@@ -3,9 +3,11 @@
 use App\Http\Controllers\SetupController;
 use Illuminate\Support\Facades\Route;
 
-// Setup ->> Store Management Route
-Route::get('setup/warehouse-setup', [SetupController::class, 'warehouseSetup'])->name('warehouse.setup');
-Route::get('setup/measure-unit-setup', [SetupController::class, 'measureUnitSetup'])->name('measure.unit.setup');
-Route::get('setup/product-category-setup', [SetupController::class, 'productCategorySetup'])->name('product.category.setup');
-Route::get('setup/product-create', [SetupController::class, 'productCreate'])->name('product.create');
-// Setup ->> Store Management Route
+Route::group(['prefix' => 'setup', 'middleware' => ['auth']], function () {
+    // Setup ->> Store Management Route
+    Route::get('warehouse-setup', [SetupController::class, 'warehouseSetup'])->name('warehouse.setup');
+    Route::get('measure-unit-setup', [SetupController::class, 'measureUnitSetup'])->name('measure.unit.setup');
+    Route::get('product-category-setup', [SetupController::class, 'productCategorySetup'])->name('product.category.setup');
+    Route::get('product-create', [SetupController::class, 'productCreate'])->name('product.create');
+    // Setup ->> Store Management Route
+});
