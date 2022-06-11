d4da3b7b05d0d2140ca138f9b7258190925e03b2
Md.yousuf cst <mdy506071@gmail.com>
2022-06-11 15:20:04 +0600

    risk assessment

--- resources/views/assessment/individual_risk_assessment_form.blade.php
+++ resources/views/assessment/individual_risk_assessment_form.blade.php
@@ -35,115 +35,17 @@
                         <li class="nav-item">
                             <a class="nav-link active" data-bs-toggle="tab" href="#case-histroy" role="tab">
                                 <span class="d-block d-sm-none"><i class="fas fa-home"></i></span>
-                                <span class="d-none d-sm-block">Individual Fisk Assessment Form</span>
+                                <span class="d-none d-sm-block">Individual Risk Assessment Form</span>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" data-bs-toggle="tab" href="#case-histroy_list" role="tab">
                                 <span class="d-block d-sm-none"><i class="far fa-user"></i></span>
-                                <span class="d-none d-sm-block">Individual Fisk Assessment List</span>
+                                <span class="d-none d-sm-block">Individual Risk Assessment List</span>
                             </a>
                         </li>
                     </ul>
-<div class="row">
-  <div class="col-12">
-    <div class="card">
-      <div class="card-body">
-        <div class="row mb-3">
-          <div class="col-6">
-            <h4 class="card-title">STUDENT QAS</h4>
-          </div>
-          <div class="col-6 text-end mb-3">
-            <button type="button" class="btn btn-outline-info waves-effect waves-light" data-bs-toggle="modal"
-              data-bs-target=""><i class="fa fa-user"></i> Profile</button>
-            <button type="button" class="btn btn-outline-info waves-effect waves-light" data-bs-toggle="modal"
-              data-bs-target=".bs-example-modal-lg"><i class="fa fa-plus"></i> ADD</button>
-          </div>
-          <div class="row">
-            <div class="col-sm-12 col-md-6">
-              <label style="display: inline-flex;align-items: center;"> Show <select name="length"
-                  class="form-control form-control-sm form-select form-select-sm">
-                  <option value="10">10</option>
-                  <option value="25">25</option>
-                  <option value="50">50</option>
-                  <option value="100">100</option>
-                </select> entries
-              </label>
-            </div>
-            <div class="col-sm-12 col-md-6 text-end">
-              <label style="display: inline-flex;align-items: center;">Search:
-                <input type="search" class="form-control form-control-sm" placeholder="">
-              </label>
-            </div>
-          </div>
-          <div class="table-responsive">
-            <table class="table table-bordered w-100">
-              <thead>
-                <tr class="table-primary">
-                  <th>Collection Date</th>
-                  <th>Student Name</th>
-                  <th>Collected By</th>
-                  <th>Log</th>
-                  <th>Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>2022-04-13</td>
-                  <td>Hossain Rafif Ahyaan</td>
-                  <td>Nurjahan Dipa</td>
-                  <td><small>Create: Nurjahan Dipa @ 2022-04-13 12:32:20</small></td>
-                  <td>
-                    <button type="button" class="btn btn-sm btn-primary btn-rounded waves-effect waves-light mb-2 me-1"
-                      data-bs-toggle="modal" data-bs-target=".bs-example-modal-lg">
-                      <i class="mdi mdi-eye"></i>
-                    </button>
-                    <button type="button" class="btn btn-sm btn-info btn-rounded waves-effect waves-light mb-2 me-1"> <i
-                        class="fas fa-check"></i>
-                    </button>
-                    <button type="button" class="btn btn-sm btn-danger btn-rounded waves-effect waves-light mb-2 me-1">
-                      <i class="fas fa-check"></i>
-                    </button>
-                    <button type="button" class="btn btn-sm btn-success btn-rounded waves-effect waves-light mb-2 me-1"
-                      data-bs-toggle="modal" data-bs-target=".bs-example-modal-lg">
-                      <i class="mdi mdi-pencil"></i>
-                    </button>
-                    <button type="button" class="btn btn-sm btn-success btn-rounded waves-effect waves-light mb-2 me-1">
-                      <i class="bx bx-dollar"></i>
-                    </button>
-                    <button type="button" class="btn btn-sm btn-success btn-rounded waves-effect waves-light mb-2 me-1">
-                      <i class="fas fa-arrow-circle-right"></i>
-                    </button>
-                    <button type="button" class="btn btn-sm btn-danger btn-rounded waves-effect waves-light mb-2">
-                      <i class="fas fa-trash-alt"></i>
-                    </button>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-          <div class="row">
-            <div class="col-sm-12 col-md-6">
-              Showing 1 to 2 of 2 entries
-            </div>
-            <div class="col-sm-12 col-md-6 text-end">
-              <nav>
-                <ul class="pagination" style="justify-content: end;">
-                  <li class="page-item disabled">
-                    <a class="page-link" href="#" tabindex="-1">Previous</a>
-                  </li>
-                  <li class="page-item"><a class="page-link" href="#">1</a></li>
-                  <li class="page-item active">
-                    <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
-                  </li>
-                  <li class="page-item"><a class="page-link" href="#">3</a></li>
-                  <li class="page-item">
-                    <a class="page-link" href="#">Next</a>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-          </div>
+
                     <!-- Tab panes -->
                     <div class="tab-content p-3 text-muted">
                         <div class="tab-pane active" id="case-histroy" role="tabpanel">
@@ -227,7 +129,6 @@
                                                             </section>
                                                         </div>
                                                         <div class="tab-pane" id="Pragmatic_Objective">
-
 
                                                             <section>
                                                                 <form>
