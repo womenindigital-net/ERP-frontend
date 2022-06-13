0dd7784e7680b98dc2b268f7136082e6894483ca
womenindigital-net <94352977+womenindigital-net@users.noreply.github.com>
2022-06-13 15:29:41 +0600

    /individual-risk and sensory-checklist-adult Repository  Created

---
+++ app/Repositories/IndividualRiskRepositories.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\IndividualRisk;
+
+class IndividualRiskRepositories extends BaseRepository
+{
+    protected string $model = IndividualRisk::class;
+}
