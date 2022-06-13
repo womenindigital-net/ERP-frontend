0308b20b656e57a4097d312d2830ff3dbde43298
Hridoy Roy <hridayroy05@gmail.com>
2022-06-13 15:05:31 +0600

    reposi

---
+++ app/Repositories/SensoryChecklistChildRepositiry.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\SensoryChecklistChild;
+
+class SensoryChecklistChildRepositiry extends BaseRepository
+{
+    protected string $model = SensoryChecklistChild::class;
+}
---
+++ app/Repositories/SocialCommunicationRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+// namespace App\Repositories;
+
+// use App\Models\SocileCommunication;
+
+// class SensoryChecklistChildRepositiry extends BaseRepository
+// {
+//     protected string $model = SensoryChecklistChild::class;
+// }
