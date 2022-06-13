7233ff3c2ccbd0363b2f326ad0f469217586ae32
womenindigital-net <94352977+womenindigital-net@users.noreply.github.com>
2022-06-13 16:10:09 +0600

    functional-communication

---
+++ app/Repositories/FunctionalCommunicationRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\FunctionalCommunication;
+
+class FunctionalCommunicationRepository extends BaseRepository
+{
+    protected string $model = FunctionalCommunication::class;
+}
