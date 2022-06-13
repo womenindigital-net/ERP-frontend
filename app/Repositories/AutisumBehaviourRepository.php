fd4f37e28e03e5f9d4b184740313fdd69aa083d9
womenindigital-net <94352977+womenindigital-net@users.noreply.github.com>
2022-06-13 15:35:50 +0600

    autisum-behaviour Repository  Created

---
+++ app/Repositories/AutisumBehaviourRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\AutisumBhehaviour;
+
+class AutisumBehaviourRepository extends BaseRepository
+{
+    protected string $model = AutisumBhehaviour::class;
+}
