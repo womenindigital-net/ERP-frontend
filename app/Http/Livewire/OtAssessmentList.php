76c7a4b48f3ac3bee574b3ca0f5c63338ffd3cd6
Md.yousuf cst <mdy506071@gmail.com>
2022-06-14 14:02:14 +0600

    livewire

---
+++ app/Http/Livewire/OtAssessmentList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class OtAssessmentList extends Component
+{
+    public function render()
+    {
+        return view('livewire.ot-assessment-list');
+    }
+}
--- resources/views/assessment/social-communication/create.blade.php
+++ resources/views/assessment/social-communication/create.blade.php
@@ -942,61 +942,7 @@
                             </div> <!-- end col -->
                         </div>
                         <div class="tab-pane " id="case-histroy_list" role="tabpanel">
-                            <!-- form start -->
-                            <div class="table-responsive">
-                                <table class="table table-bordered w-100">
-                                    <thead>
-                                        <tr class="table-primary">
-                                            <th>Collection Date</th>
-                                            <th>Student Name</th>
-                                            <th>Collected By</th>
-                                            <th>Log</th>
-                                            <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>2019-02-03</td>
-                                            <td>Omiggg</td>
-                                            <td>Karishma Cultural</td>
-                                            <td><small>Create: Karishma Cultural @ 2019-02-17 18:32:43</small></td>
-                                            <td>
-                                                <button type="button"
-                                                    class="btn btn-sm btn-primary btn-rounded waves-effect waves-light mb-2 me-1"
-                                                    data-bs-toggle="modal" data-bs-target=".social-communication-view">
-                                                    <i class="mdi mdi-eye"></i>
-                                                </button>
-                                                <button type="button"
-                                                    class="btn btn-sm btn-info btn-rounded waves-effect waves-light mb-2 me-1">
-                                                    <i class="fas fa-check"></i>
-                                                </button>
-                                                <button type="button"
-                                                    class="btn btn-sm btn-danger btn-rounded waves-effect waves-light mb-2 me-1">
-                                                    <i class="fas fa-check"></i>
-                                                </button>
-                                                <button type="button"
-                                                    class="btn btn-sm btn-success btn-rounded waves-effect waves-light mb-2 me-1"
-                                                    data-bs-toggle="modal" data-bs-target=".social-communication-view">
-                                                    <i class="mdi mdi-pencil"></i>
-                                                </button>
-                                                <button type="button"
-                                                    class="btn btn-sm btn-success btn-rounded waves-effect waves-light mb-2 me-1">
-                                                    <i class="bx bx-dollar"></i>
-                                                </button>
-                                                <button type="button"
-                                                    class="btn btn-sm btn-success btn-rounded waves-effect waves-light mb-2 me-1">
-                                                    <i class="fas fa-arrow-circle-right"></i>
-                                                </button>
-                                                <button type="button"
-                                                    class="btn btn-sm btn-danger btn-rounded waves-effect waves-light mb-2">
-                                                    <i class="fas fa-trash-alt"></i>
-                                                </button>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                            <!-- end row -->
+                             <livewire:social-communication-list/>
                         </div>
                     </div>
 
