e599dc787ef07ba1fb8ae00fd1be3c848aaeaa33
Md.yousuf cst <mdy506071@gmail.com>
2022-06-19 11:57:15 +0600

    notice board

---
+++ resources/views/program/notice-board/view.blade.php
@@ -0,0 +1,110 @@
+<div class="modal-body">
+    <div class="mb-3 row">
+        <label for="example-search-input" class="col-md-4 col-form-label text-center">Send
+            To</label>
+        <div class="col-md-8 pe-5 ">
+            <div class="border p-2">
+                <span class="border m-2">Digital</span>
+                <span class="border m-2">Dev</span>
+                <span class="border m-2">COMMON</span>
+                <span class="border m-2">Rahman</span>
+                <span class="border m-2">Dipa</span>
+                <span class="border m-2">Parvez</span>
+                <span class="border m-2">Hossain</span>
+                <span class="border m-2">Jahan</span>
+                <span class="border m-2">Amin</span>
+                <span class="border m-2">Rahman</span>
+                <span class="border m-2">Azim</span>
+                <span class="border m-2">Shiddique</span>
+                <span class="border m-2">Digital</span>
+                <span class="border m-2">Dev</span>
+                <span class="border m-2">COMMON</span>
+                <span class="border m-2">Rahman</span>
+                <span class="border m-2">Dipa</span>
+                <span class="border m-2">Parvez</span>
+                <span class="border m-2">Hossain</span>
+                <span class="border m-2">Jahan</span>
+                <span class="border m-2">Amin</span>
+                <span class="border m-2">Rahman</span>
+                <span class="border m-2">Azim</span>
+                <span class="border m-2">Shiddique</span>
+                <span class="border m-2">Digital</span>
+                <span class="border m-2">Dev</span>
+                <span class="border m-2">COMMON</span>
+                <span class="border m-2">Rahman</span>
+                <span class="border m-2">Dipa</span>
+                <span class="border m-2">Parvez</span>
+                <span class="border m-2">Hossain</span>
+                <span class="border m-2">Jahan</span>
+                <span class="border m-2">Amin</span>
+                <span class="border m-2">Rahman</span>
+                <span class="border m-2">Azim</span>
+                <span class="border m-2">Shiddique</span>
+                <span class="border m-2">Digital</span>
+                <span class="border m-2">Dev</span>
+                <span class="border m-2">COMMON</span>
+                <span class="border m-2">Rahman</span>
+                <span class="border m-2">Dipa</span>
+                <span class="border m-2">Parvez</span>
+                <span class="border m-2">Hossain</span>
+                <span class="border m-2">Jahan</span>
+                <span class="border m-2">Amin</span>
+                <span class="border m-2">Rahman</span>
+                <span class="border m-2">Azim</span>
+                <span class="border m-2">Shiddique</span>
+            </div>
+        </div>
+    </div>
+    {{-- subject --}}
+    <div class="mb-3 row">
+        <label for="example-search-input" class="col-md-4 col-form-label text-center">Subject</label>
+        <div class="col-md-8 pe-5">
+            <input class="form-control" type="search" placeholder="Holiday Notice" id="example-search-input">
+        </div>
+    </div>
+    {{-- massege --}}
+    <div class="mb-3 row">
+        <label for="example-search-input" class="col-md-4 col-form-label text-center">Send
+            To</label>
+        <div class="col-md-8 pe-5 ">
+            <div class="border p-2">
+                <p>সুত্রঃ পিএফডিএ-ভিটিসি/প্রশাসন-০১/খন্ড-৫/সং-০১/২০২১ ইং তারিখঃ
+                    ২৯/০৮/২০২১ ইং</p>
+                <h2 class="text-center text-primary">ছুটির নোটিশ</h2>
+                <p>এতদ্বারা সেন্টারের সকল কর্মকর্তা, কর্মচারী ও
+                    প্রশিক্ষণার্থীদের সদয় অবগতির জন্য
+                    জানানো
+                    যাচ্ছে যে, আগামী ৩০শে আগষ্ট-২০২১ ইং রোজ সোমবার, জন্মাষ্টমী
+                    উপলক্ষে
+                    পিএফডিএ-ভোকেশনাল
+                    ট্রেনিং সেন্টার ট্রাষ্ট এর সকল কার্যক্রম বন্ধ থাকবে।
+                    <br><br>
+
+                    ইহা ছাড়াও উপরোক্ত ছুটিকালীন সময়ে প্রোডাকশন বিভাগ, নিরাপত্তা
+                    বিভাগ এবং সেকশন হতে
+                    দায়িত্ব প্রাপ্ত ব্যাক্তি উপরোক্ত ছুটি কালীন সময়ে অফিসের
+                    প্রয়োজনে কর্তব্য পালন
+                    করবে।<br><br> <br>
+
+                    আগামী ৩১শে আগষ্ট-২০২১ ইং রোজ মঙ্গলবার নির্ধারিত সময় অনুযায়ী
+                    পিএফডিএ-ভোকেশনাল
+                    ট্রেনিং
+                    সেন্টার ট্রাষ্ট এর সকল সেকশন/বিভাগ খোলা থাকবে। অতএব সকলকে
+                    নির্ধারিত সময়ে উপস্থিত
+                    থাকার জন্য নির্দেশ প্রদান করা হলো।<br> <br>
+                    ----------------------- <br>
+                    সাজিদা রহমান ড্যানি <br>
+                    পিএফডিএ-ভোকেশনাল ট্রেনিং সেন্টার ট্রাষ্ট <br>
+                    নিউ ডিওএইচএস, মহাখালী, ঢাকা-১২০৬
+                </p>
+            </div>
+        </div>
+    </div>
+
+</div>
+<div class="row modal-footer">
+
+    <div class="col-md-1">
+        <button class="btn btn-danger w-100" data-bs-dismiss="modal">Close</button>
+    </div>
+</div>
