12c8746e6044c049585d02f0077cd2b1c01785f4
Sohag Hossain <shcseilacs@gmail.com>
2022-07-11 01:26:43 +0600

    itp wip

---
+++ app/Models/Mitp.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mitp extends Model
+{
+    use HasFactory;
+}
