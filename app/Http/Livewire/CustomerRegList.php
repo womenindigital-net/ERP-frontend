d3984c44c17308ef809d82e986a20ca7acd974f9
Bidhan Deb Nath <Bidhandebnath15.09.2001@gmail.com>
2022-07-17 17:54:20 +0600

    Add

---
+++ app/Http/Livewire/CustomerRegList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class CustomerRegList extends Component
+{
+    public function render()
+    {
+        return view('livewire.customer-reg-list');
+    }
+}
