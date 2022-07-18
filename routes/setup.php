e6b6ead77abb0f383215d07db66bf5a637a15a34
womenindigital-net <alwayskuhelikapuja@gmail.com>
2022-07-18 12:42:33 +0600

    account setup

--- routes/setup.php
+++ routes/setup.php
@@ -2,8 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SetupController;
+use App\Http\Controllers\CustomerTypeSetupController;
 
 Route::group(['prefix' => 'setup', 'middleware' => ['auth']], function () {
+    Route::resources([
+        'customer-Type' => CustomerTypeSetupController::class,
+        
+    ]);
     // Setup ->> Store Management Route
     Route::get('warehouse-setup.create', [SetupController::class, 'warehouseSetup'])->name('warehouse.setup.create');
     Route::get('measure-unit-setup.create', [SetupController::class, 'measureUnitSetup'])->name('measure.unit.setup.create');
@@ -45,7 +50,8 @@ Route::group(['prefix' => 'setup', 'middleware' => ['auth']], function () {
     Route::get('vendor-list/supplier-type-setup.create', [SetupController::class, 'supplierTypeSetup'])->name('setup.vendor-list.supplier-type-setup.create');
     // Customer List 
     Route::get('customers-list/customer-reg/create', [SetupController::class, 'customerReg'])->name('setup.customers-list.customer-reg.create');
-    Route::get('customers-list/customer-type/create', [SetupController::class, 'customerType'])->name('setup.customers-list.customer-type.create');
+    // Route::get('customers-list/customer-type/create', [SetupController::class, 'customerType'])->name('setup.customers-list.customer-type.create');
+   
     // Course and Admission
     Route::get('course-and-admission.create', [SetupController::class, 'courseAdmission'])->name('setup.course-and-admission.create');
 
