6a92c8ac4626286b676861b4fe1a05685d65cc8b
Hridoy Roy <hridayroy05@gmail.com>
2022-07-20 15:50:58 +0600

    stock receive

--- app/Http/Livewire/StockReceiveCreate.php
+++ app/Http/Livewire/StockReceiveCreate.php
@@ -8,6 +8,7 @@ use App\Services\ProductService;
 use App\Repositories\UserRepository;
 use App\Repositories\StockRepository;
 use App\Services\StockReceiveService;
+use App\Repositories\ProductRepository;
 use App\Repositories\ProjectRepository;
 use App\Repositories\PurchaseRepository;
 use App\Repositories\WarehouseRepository;
@@ -35,13 +36,12 @@ class StockReceiveCreate extends Component
     public $stock_receive_qty;
     public $serial;
     public $type;
-    public $purchaseInput;
-    public $receiveInput;
     public $purchaseProduct;
 
     private StockReceiveService $service;
     private StockReceiveRepository $repo;
     private ProductService $productService;
+    private ProductRepository $productRepo;
     private PurchaseOrderRepository $purchaseOrderRepo;
     private ProjectRepository $projectRepo;
     private UserRepository $userRepo;
@@ -54,6 +54,7 @@ class StockReceiveCreate extends Component
         StockReceiveService $service,
         StockReceiveRepository $repository,
         ProductService $productService,
+        ProductRepository $productRepo,
         ProjectRepository $projectRepository,
         PurchaseOrderRepository $purchaseOrderRepository,
         UserRepository $userRepository,
@@ -62,12 +63,11 @@ class StockReceiveCreate extends Component
         PurchaseRepository $purchaseRepository,
     ) {
         $this->inputs[] = $this->numberOfItems;
-        // $this->receiveInput = false;
-        // $this->purchaseInput = false;
         $this->purchaseOrderRepo = $purchaseOrderRepository;
         $this->service = $service;
         $this->repo = $repository;
         $this->productService = $productService;
+        $this->productRepo = $productRepo;
         $this->projectRepo = $projectRepository;
         $this->userRepo = $userRepository;
         $this->warehouseRepository = $warehouseRepository;
@@ -81,35 +81,17 @@ class StockReceiveCreate extends Component
         $this->purchaseProduct = [];
     }
 
-    // public function updating($name, $value)
-    // {
-    //     if ($name == 'type') {
-    //         if ($value == 'purchase') {
-    //             $this->purchaseInput = true;
-    //             $this->receiveInput = false;
-    //         } elseif ($value == 'return') {
-    //             $this->receiveInput = true;
-    //             $this->purchaseInput = false;
-    //         } else {
-    //             $this->receiveInput = false;
-    //             $this->purchaseInput = false;
-    //         }
-    //     }
-    // }
 
     public function mount()
     {
         if ($this->stockReceive) {
-            // dd($this->stockReceive);
             $this->stockReceive = $this->repo->getRelatedData($this->stockReceive, ['details']);
 
             $this->project_id = $this->stockReceive->project_id;
             $this->type = $this->stockReceive->type;
             $this->purchase_type = $this->stockReceive->purchase_type;
             $this->purchase_id = $this->stockReceive->purchase_id;
-            $this->purchase_type ? $this->purchaseInput = true : $this->purchaseInput = false;
             $this->return_type = $this->stockReceive->return_type;
-            $this->return_type ? $this->receiveInput = true : $this->receiveInput = false;
             $this->date = $this->stockReceive->date;
             $this->warehouse_id = $this->stockReceive->warehouse_id;
 
@@ -148,7 +130,6 @@ class StockReceiveCreate extends Component
             }
 
             $this->available_qty[$targetKey] = $productInfo->qty;
-            // $this->price[$targetKey]         = $productInfo->product->selling_price;
         }
 
         if (str_starts_with($name, 'purchase_id')) {
@@ -158,7 +139,6 @@ class StockReceiveCreate extends Component
         if (str_starts_with($name, 'received.')) {
             $targetKey = $this->getTargetKey($name);
             $this->stock_receive_qty[$targetKey] = $value;
-            // dd($stock_receive_qty);
         }
 
         if (str_starts_with($name, 'return.')) {
@@ -190,40 +170,12 @@ class StockReceiveCreate extends Component
         $this->redirectRoute('stock-receive.create');
     }
 
-    // public function updated($name, $value)
-    // {
-    //     if (str_starts_with($name, 'product_id.')) {
-    //         dd($name);
-    //         if (!$value || !$this->project_id || !$this->warehouse_id)
-    //             return;
-
-    //         $productInfo = $this->stockRepo->getDetailAccordingly($this->project_id, $this->warehouse_id, $value);
-    //         if (!$productInfo) return;
-
-    //         $targetKey = $this->getTargetKey($name);
-
-    //         $this->available_qty[$targetKey] = $productInfo->qty;
-    //         $this->price[$targetKey] = $productInfo->product->selling_price;
-    //     }
-
-    //     if (str_starts_with($name, 'qty.') || str_starts_with($name, 'discount.')) {
-    //         $targetKey = $this->getTargetKey($name);
-
-    //         if (!isset($this->available_qty[$targetKey]) or !$this->available_qty[$targetKey])
-    //             return;
-
-    //         if (isset($this->price[$targetKey]) && $this->price[$targetKey] && isset($this->qty[$targetKey]) && $this->qty[$targetKey]) {
-    //             $this->sub_total[$targetKey] = ($this->price[$targetKey] * $this->qty[$targetKey]) - ($this->discount[$targetKey] ?? 0);
-    //         }
-    //     }
-    // }
-
     public function render()
     {
 
         $data = [
             'projects' => $this->projectRepo->getData(),
-            'products' => $this->productService->getFormattedDataAsOptGroup(),
+            'products' => $this->productRepo->getData(),
             'users' => $this->userRepo->getData(),
             'warehouses' => $this->warehouseRepository->getData(),
             'purchaseOrder' => $this->purchaseOrderRepo->getData(),
