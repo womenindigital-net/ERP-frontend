4746e13c0f90219dc8b35a599cb6b8ef861b0a9e
womenindigital-net <alwayskuhelikapuja@gmail.com>
2022-07-20 16:41:56 +0600

    add

---
+++ app/Http/Livewire/YearEndCloseList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class YearEndCloseList extends Component
+{
+    public function render()
+    {
+        return view('livewire.year-end-close-list');
+    }
+}
