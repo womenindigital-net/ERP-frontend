6fc456d2413eea95a6fabd6480441c4295e1eb06
Md.yousuf cst <86878359+Md-yousuf-bd@users.noreply.github.com>
2022-07-24 13:43:13 +0600

    product

--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -17,4 +17,14 @@ class ProductRepository extends BaseRepository
     {
         return $this->model::specifyingRelation()->get();
     }
+    public function getListData($perPage, $search)
+    {
+        return $this->model::when($search, function ($query) use ($search) {
+            $query->where("address", "like", "%$search%")
+            ->orWhere("email", "like", "%$search%")
+            ->orWhere("phone", "like", "%$search%")
+                //                  ->orWhere('student.name', 'like', "%$search%")
+            ;
+        })->latest()->paginate($perPage);
+    }
 }
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -313,7 +313,7 @@
                                 <li><a href="{{ route('product-category.create') }}">Product Category Setup</a>
                                 </li>
 
-                                <li><a href="{{ route('product.create.create') }}">Product Create</a></li>
+                                <li><a href="{{ route('product.create') }}">Product Create</a></li>
                             </ul>
                         </li>
                         <li>
